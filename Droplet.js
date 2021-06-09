bfb55fa64f585188fc5f952ca37458e44e50402e
SaiBlaze <84612731+SaiBlaze@users.noreply.github.com>
2021-06-09 14:38:00 +0530

    Add files via upload

---
+++ Droplet.js
@@ -0,0 +1,24 @@
+class Droplet{
+    constructor(x,y){
+      var option = {
+
+        friction : 0.001,
+        restitution : 0.1
+      }
+      this.radius = 5
+      this.rain = Bodies.circle(x,y,5,option)
+      World.add (world,this.rain)
+
+    }
+    updateY(){
+
+        if(this.rain.position.y>height){
+          Matter.Body.setPosition(this.rain,{x:random(0,400),y:random(0,400)})
+        }
+    }
+    showDrop(){
+      fill("blue")
+      ellipseMode(CENTER)
+      ellipse(this.rain.position.x,this,position.y,this.radius,this.radius)
+    } 
+}
